ad42250cd9f85ac7da3c74786c2933ceb4bc3349
ElErizio <theericyea100@hotmail.com>
2025-10-23 19:04:10 -0600

    Cartas de Loteria y SO
    Cartas de Loteria para integrar y Scriptable Objects

--- Moncho Master/Assets/_Scripts/Sistemas/Crafteo/IngredientSO.cs
+++ Moncho Master/Assets/_Scripts/Sistemas/Crafteo/IngredientSO.cs
@@ -7,13 +7,13 @@ public class IngredientSO : ScriptableObject
     [SerializeField] private string displayName;
     [SerializeField] private Sprite icon;
     [SerializeField] private IngredientCategory category;
-    [SerializeField] private string[] tags;
     [SerializeField] private bool isUnlocked = true;
+    [SerializeField] private Sprite card;
 
     public string Id => id;
     public string DisplayName => displayName;
     public Sprite Icon => icon;
+    public Sprite Card => card;
     public IngredientCategory Category => category;
-    public string[] Tags => tags;
     public bool IsUnlocked => isUnlocked;
 }
