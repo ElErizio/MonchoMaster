c4f5885200aaa08e5c461fdbc5ce578a0485a588
Eric I. Rosas <theericyea100@hotmail.com>
2025-11-12 23:52:01 -0600

    Escena de Créditos
    Escena de créditos con movimiento, roles y nombres

---
+++ Moncho Master/Assets/_Scripts/UI/CreditsRoller.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+public class CreditsRoller : MonoBehaviour
+{
+    [Header("Configuración")]
+    public float speed = 50f;
+    public string mainMenuSceneName = "MainMenu";
+    public float endPositionY = 1500f;
+
+    [Header("Referencias")]
+    public RectTransform creditsContainer;
+
+    void Update()
+    {
+        creditsContainer.anchoredPosition += Vector2.up * speed * Time.deltaTime;
+
+        bool escapePressed = Keyboard.current != null && Keyboard.current.escapeKey.wasPressedThisFrame;
+        bool mouseClicked = Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame;
+
+        if (creditsContainer.anchoredPosition.y > endPositionY || escapePressed || mouseClicked)
+        {
+            ReturnToMenu();
+        }
+    }
+
+    void ReturnToMenu()
+    {
+        SceneManager.LoadScene(mainMenuSceneName);
+    }
+}
